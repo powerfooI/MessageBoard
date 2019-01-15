44d3c82ae70ef1c9c0032fca7cf9c01a7d2683e7
WSY <sy_wang16@163.com>
2019-01-16 00:12:19 +0800

    only index left

--- board/views.py
+++ board/views.py
@@ -6,7 +6,7 @@ from django.urls import reverse
 from .models import Message
 
 def index(request, page_num=1):
-    start = (page_num-1)*5
+    start = (page_num-1)*10
     latest_message_list = Message.objects.filter(is_checked=True).order_by('-send_time')[start:start+10]
     if page_num == 1:
         last_page = 1
@@ -24,26 +24,27 @@ def index(request, page_num=1):
     return render(request, 'board/index.html', context)
 
 def manage(request, page_num=1):
-    start = (page_num-1)*5
+    start = (page_num-1)*10
     need_to_check_list = Message.objects.order_by('-send_time')[start:start+10]
     if page_num == 1:
         last_page = 1
     else:
         last_page = page_num - 1
-    if len(need_to_check_list) < 10:
+    if len(need_to_check_list) <= 10:
         next_page = page_num
     else:
         next_page = page_num + 1
     context = {
         'need_to_check_list': need_to_check_list,
-        'next_page': last_page,
-        'last_page': next_page,
+        'next_page': next_page,
+        'last_page': last_page,
         'current_page': page_num,
     }
     return render(request, 'board/manage.html', context)
 
 def write_message(request):
     if request.method == 'POST':
+        print('POST DICT', request.POST)
         new_message = Message()
         new_message.message_text = request.POST['text-box']
         new_message.wechat_id = request.POST['wechat-box']
