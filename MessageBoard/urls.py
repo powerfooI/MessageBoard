ddc2667f8ed873d8c7767f3215883250c58f7c04
WSY <sy-wang16@mails.tsinghua.edu.cn>
2019-01-16 17:49:18 +0800

    need to add admin user page?

--- MessageBoard/urls.py
+++ MessageBoard/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('board/', include('board.urls')),
+    path('', include('board.urls')),
     path('admin/', admin.site.urls),
 ]
--- board/urls.py
+++ board/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = 'board'
 urlpatterns = [
     path('index/', views.index, name='index'),
-    path('manage/', views.manage, name='manage'),
+    path('manage_the_message/', views.manage, name='manage'),
     path('index/<int:page_num>/', views.index, name='index'),
-    path('manage/<int:page_num>/', views.manage, name='manage'),
+    path('manage_the_message/<int:page_num>/', views.manage, name='manage'),
     path('write_message/', views.write_message, name='write_message'),
     path('check_pass/<int:message_id>/<int:page_num>/', views.check_pass, name='check_pass'),
     path('detail/<int:message_id>/<int:page_num>/', views.detail, name='detail'),
